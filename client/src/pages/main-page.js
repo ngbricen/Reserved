77d983e95af96eda820b9feb20dc8d250c5311e4
David Rosenberg <dsr0116@yahoo.com>
2017-10-29 20:27:14 -0400

    updated main page to call yelp API: have to configure Chrome to disable same origin policy to get response locally....yelp uses OAuth 2

--- client/src/pages/main-page.js
+++ client/src/pages/main-page.js
@@ -1,29 +1,71 @@
 import React, { Component } from "react";
+import yelp from "../keys/yelp";
+import $ from "jquery";
 
 class MainPage extends Component {
+	state = {
+		businesses: []
+	}
+
+	yelp = event => {
+		let business = [];
+		let search = "clubs";
+		let location = "Atlanta+GA";
+		if ($("#search").val()) search = $("#search").val().trim();
+		if ($("#location").val()) location = $("#location").val().trim();
+		var data = null;
+
+		var xhr = new XMLHttpRequest();
+		xhr.withCredentials = true;
+
+		xhr.addEventListener("readystatechange", function () {
+		  if (this.readyState === 4) {
+		  	const resp = JSON.parse(this.responseText);
+
+		    resp.businesses.map(resp => {
+		    	business.push(resp);
+		    })
+		  }
+		});
+
+		xhr.open("GET", "https://api.yelp.com/v3/businesses/search?term="+search+"&location="+location);
+		xhr.setRequestHeader("authorization", "Bearer "+yelp.Token);
+		xhr.setRequestHeader("cache-control", "no-cache");
+		xhr.setRequestHeader("postman-token", yelp.Postman);
+
+		xhr.send(data);
+		console.log(business);
+		this.setState({ businesses: business });
+		setTimeout(() => {
+			console.log(this.state.businesses);
+		}, 2000);
+		
+	}
 
   render() {
     return (
         <div className="body">
-          <form className="search">
+          <div className="search">
 
             <div className="searchCont">
 
               <div className="searchField">
                 <span className="find">Find</span>
-                <input name="search" placeholder="Nighlife, drinks, dancing...." />
+                <input name="search" placeholder="Nighlife, drinks, dancing...." id="search" />
               </div>
 
               <div className="placeField">
                 <span className="near">Near</span>
-                <input name="place" placeholder="Atlanta, GA" />
+                <input name="place" placeholder="Atlanta, GA" id="location" />
               </div>
+
+              <button type="submit" className="btn btn-danger" id="searchSub" onClick={this.yelp}><i className="fa fa-search fa-lg" aria-hidden="true"></i></button>
 
               <div className="clear"></div>
 
             </div>
 
-          </form>
+          </div>
         </div>
 	  )
 	}
--- keys/yelp.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	Client_Id: "rNHadZw1wTEexrwRvMXYrg",
-	Client_Secret: "9GdkwR5WJVBYYlcnVDmomhvUfQ9ALHT3aSqe06a40ZndDyZbhhQPNqR3KLkdIZHe",
-	Token: "QBUS9BXk1uVMA_MaoCXrOCVyV5pYZQVMaTjy8ZG-_q0VdjfjqOwSgvKWsEl41DzhGi6KI7cq1BZ0VlUSUAk13HSUXfhzn4vawXE1jqC6m0Uyc56Vzi5f8voH0LX0WXYx"
-}
